3a1ad750a5f43c829e91b9985c6b85d80db54de3
a-little-bug-be-careful <2268542764@qq.com>
2022-11-03 14:28:49 +0800

    测试注释删除

--- src/main/java/com/example/demo2/controller/HelloController.java
+++ src/main/java/com/example/demo2/controller/HelloController.java
@@ -49,7 +49,6 @@ public class HelloController {
     @PostMapping("/test1")
     @ResponseStatus(HttpStatus.FOUND)
     public InvokeResponse test1() {
-        //测试更新代码
         return InvokeResponse.succ("succ");
     }
 }
