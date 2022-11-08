8865eae845c6f9dccbace0b849ee6040f257605b
a-little-bug-be-careful <2268542764@qq.com>
2022-11-08 17:35:24 +0800

    rabbitmq补充自定义rabbittemplate

--- src/main/java/com/example/demo2/config/RabbitMqConfig.java
+++ src/main/java/com/example/demo2/config/RabbitMqConfig.java
@@ -2,13 +2,14 @@ package com.example.demo2.config;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 使用 JSON 配置rabbitmq消息队列的序列化和反序列化
+ * 使用 JSON 配置rabbitmq消息队列的序列化和反序列化，不需要java对象再必须实现Serializable接口
  */
 @Configuration
 public class RabbitMqConfig {
@@ -23,4 +24,15 @@ public class RabbitMqConfig {
         factory.setMessageConverter(new Jackson2JsonMessageConverter());
         return factory;
     }
+
+    /**
+     * 可以自定义rabbitTemplate，同时定义消息序列化方式为Jackson
+     * @return
+     */
+/*    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }*/
 }
