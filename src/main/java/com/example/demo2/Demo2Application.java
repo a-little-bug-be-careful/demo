cc027dd064c47c4096c68401428837bfebf82add
Y05926 <jiangyu@lusterinc.com>
2022-12-13 15:35:51 +0800

    springcloud整合nacos

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -3,9 +3,11 @@ package com.example.demo2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.demo2.mapper")
+@EnableDiscoveryClient//通过 Spring Cloud 原生注解 @EnableDiscoveryClient 开启服务注册发现功能，经测试不加这个注解也可以被nacos注册中心发现
 public class Demo2Application {
 
     public static void main(String[] args) {
